5acad14327b00c85d6dac39c7eea3d37fa0d32a2
nb90785 <nb90785@navercorp.com>
2023-05-12 12:03:49 +0900

    메인페이지 환영문구 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Counter from '@/components/Counter';
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
 
 export default function Home() {
   console.log('Hello server');
   return (
     <>
-      <h1>Home</h1>
+      <h1>멋진걸 파는 홈페이지 입니다.</h1>
       <Counter />
       <Image src="https://images.unsplash.com/photo-1441986300917-64674bd600d8" alt="shop" width={400} height={400} />
     </>
